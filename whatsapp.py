5bc3f50b98810d88e843bd94f8b40cd6b7ef61b2
Marquise Wheeler <mardanmw27+LUIT@gmail.com>
2022-06-30 20:40:11 -0500

    First Whatsapp script

---
+++ whatsapp.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env Python3
+
+import pywhatkit
+
+# syntax: phone number with country code, message, hour and minutes
+pywhatkit.sendwhatmsg('+12053056961', "Hello kid 1", 20, 38, 00, True, 2)
